8f4c72fe4d24b32e620e70288466c8b5d8fa13f7
thedig <dig@monsoonco.com>
2014-09-20 16:57:01 -0700

    Adds stories table.  Converts main page to haml.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920214230) do
+ActiveRecord::Schema.define(version: 20140920230124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20140920214230) do
     t.string   "name"
     t.string   "url"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stories", force: true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "date_published"
+    t.string   "description"
+    t.string   "story_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
